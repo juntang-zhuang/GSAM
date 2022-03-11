bd6e2ba4c18bdc8860e079f6a407bc523752f2a2
Juntang Zhuang <44451229+juntang-zhuang@users.noreply.github.com>
2022-03-11 17:14:34 -0500

    Update train.py

--- example/train.py
+++ example/train.py
@@ -78,7 +78,7 @@ if __name__ == "__main__":
                 correct = torch.argmax(predictions.data, 1) == targets
                 log(model, loss.cpu().repeat(args.batch_size), correct.cpu(), scheduler.lr())
                 scheduler(epoch)
-                optimizer.rho_scheduler.step()
+                optimizer.update_rho_t()
 
         model.eval()
         log.eval(len_dataset=len(dataset.test))
